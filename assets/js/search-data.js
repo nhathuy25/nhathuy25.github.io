1d3af6dc851270dd18cf31a8e37847c344c11db5
nhathuy25 <120752230+nhathuy25@users.noreply.github.com>
2025-05-02 19:45:25 +0000

    Deploying to gh-pages from @ nhathuy25/nhathuy25.github.io@2b6362531093ff3af7e49029eeda90027b5040a5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,20 +4,20 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-about",
-    title: "about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
   },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
+          title: "Projects",
+          description: "Some academic and personal projects that I&#39;ve done, to be updated! See more at my github account.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
           },
         },{id: "nav-cv",
-          title: "cv",
+          title: "CV",
           description: "You can find most of my informations and experiences below, there&#39;s also a PDF version on the right of the page. Feel free to contact with me through my personal email or my LinkedIn account.",
           section: "Navigation",
           handler: () => {
@@ -394,7 +394,7 @@ ninja.data = [{
             },},{id: "news-i-joined-laboratoire-informatique-fondamentale-et-appliquee-de-tours-lifat-for-a-4-month-internship-on-computer-vision-and-deep-learning",
           title: 'I joined Laboratoire Informatique Fondamentale et Appliquee de Tours (LIFAT) for a 4-month...',
           description: "",
-          section: "News",},{id: "news-i-joined-orange-innovation-as-a-research-engineer-intern-on-energy-efficient-large-language-models-with-hpc",
+          section: "News",},{id: "news-i-joined-orange-innovation-as-a-research-engineer-intern-on-energy-efficient-large-language-models",
           title: 'I joined Orange Innovation as a Research Engineer Intern on Energy-efficient Large Language...',
           description: "",
           section: "News",handler: () => {
@@ -402,51 +402,16 @@ ninja.data = [{
             },},{id: "news-i-m-searching-actively-for-a-phd-position-after-finishing-my-internship-from-october-2025-smile",
           title: 'I’m searching actively for a PhD position after finishing my internship, from October...',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-expanding-vae",
+          title: 'Expanding VAE',
+          description: "Progressive Expansion of MLP Layers in Variational Autoencoders",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-iot-badge-system",
+          title: 'IoT Badge System',
+          description: "2-layer Authentication IoT System (Badge + Visual Recognition)",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
